c026c69260a04a63e94a5666a7cc65dc979611b3
shivaajay54 <shivaajay54@gmail.com>
2024-05-08 20:58:30 +0000

    files

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,10 +37,56 @@ const blurHeader = () =>{
 window.addEventListener('scroll', blurHeader)
 
 /*=============== SHOW SCROLL UP ===============*/ 
-
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollDown = window.scrollY
 
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '40px',
+    opacity: 1,
+    scale: 1.1,
+    duration: 2500,
+    delay: 300,
+})
+
+sr.reveal('.home_data, about__img, about_data, .visit__data')
+
+sr.reveal('.home__image, .footer__img-1, .footer__img-2', { rotate: {z: -15} })
+sr.reveal('.home_bread, about_bread', { rotate: {z: 15} })
+sr.reveal('.home__footer', { scale: 1, origin: 'bottom' })
+
+sr.reveal('.new__card:nth-child(1) img', { rotate: {z: -30}, distance: 0})
+sr.reveal('.new__card:nth-child(2) img', { rotate: {z: 15}, distance: 0, delay: 600})
+sr.reveal('.new__card:nth-child(3) img', { rotate: {z: -30}, distance: 0, delay: 900})
+
+sr.reveal('.favorite__card img', { interval: 100, rotate: {z: 15}, distance: 0 })
+
+sr.reveal('.footer__container', { scale: 1 })
